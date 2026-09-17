123f78127fe84d34603425d2e5c0490bbf37a5fe
EulloJohnRaven <cancergaming123456@gmail.com>
2026-09-18 01:25:19 +0800

    Added project.cs

---
+++ Portfolio/Models/Project.cs
@@ -0,0 +1,12 @@
+﻿namespace Portfolio.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string FullDescription { get; set; }
+        public string GitHubLink { get; set; }
+        public string ThumbnailUrl { get; set; }
+    }
+}
--- Portfolio/obj/Debug/net10.0/Portfolio.AssemblyInfo.cs
+++ Portfolio/obj/Debug/net10.0/Portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19a61feacd0e8b4c4eed92818ac21d31bd5edc50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+918b5f7a411cc8ce3be79ad5a609eb6dbb85ae9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
