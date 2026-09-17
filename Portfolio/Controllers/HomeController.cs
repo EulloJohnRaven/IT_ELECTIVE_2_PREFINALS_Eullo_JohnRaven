26cf20eef0a44000f85b0f91364c565a84dbdee0
EulloJohnRaven <cancergaming123456@gmail.com>
2026-09-18 07:54:57 +0800

    Updated HomeController

--- Portfolio/Controllers/HomeController.cs
+++ Portfolio/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Portfolio.Data;
 using Portfolio.Models;
 using System.Diagnostics;
 
@@ -6,9 +7,20 @@ namespace Portfolio.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var projects = MockDataStore.Projects;
+
+            ViewBag.IsLoggedIn = HttpContext.Session.GetString("IsLoggedIn") == "true";
+
+            return View(projects);
         }
 
         public IActionResult Privacy()
