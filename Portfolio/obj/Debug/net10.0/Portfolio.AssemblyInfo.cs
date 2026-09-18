0c22ceed64e4660225d4be3efd5ddf6edcd2bad7
EulloJohnRaven <cancergaming123456@gmail.com>
2026-09-18 08:18:55 +0800

    Designed UI and other parts

--- Portfolio/obj/Debug/net10.0/Portfolio.AssemblyInfo.cs
+++ Portfolio/obj/Debug/net10.0/Portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+072c0c4b9a180bee5726f1427c79ece0978629d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa836a18785aa04848286af254506ba563351b03")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
